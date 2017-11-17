803331f2d3b5341350997a2f8d50714aee924549
Caps <anywhere.acc@gmail.com>
2017-11-17 14:58:58 -0400

    ultimo luis

---
+++ programa.php
@@ -0,0 +1,25 @@
+<?php
+  $alumnos = [
+    'michelle' => 'nmicht',
+    'claudia'=>'claudiasg',
+    'Sergio Ochoa' => 'gnujavasergio',
+    'claudia'=>'claudiasg',
+    'Cesar Herbas' => 'R0byH',
+    'numa' => 'numa000',
+    'carlosOrtube' => 'cofran31',
+    'Sergio Ochoa' => 'gnujavasergio',
+    'pablex' => 'juanpabloraya',
+    'numa navarro' => 'lfnna1977',
+    'carlosOrtube' => 'cofran31',
+    'Sergio Ochoa' => 'gnujavasergio',
+    'Alejandra Sivila' => 'ATIXELA',											
+    'Jorge Escobar' => 'metalgeorge'
+    'Carlos Montellano' => 'cmontellano',
+    'juan_pablo' => 'pabloblass',
+    'Irving Coronel' = 'IRANT27',
+    'Edgar Paredes' => 'edgargregori',
+    'juan_pablo' => 'pabloblass',
+    'juan_navarro' => 'georgeguitar',
+    'RBethsy' => 'Bethsy',
+    'Luis Caballero' => 'zhero0',
+  ];
